801517c3615fdeb2a795eac478eb696d7761987f
Catherine Tan <catherine95118@gmail.com>
2026-02-25 10:42:15 -0800

    refactor upload_dataset, link measurement dataset to file dataset

--- crucible/parsers/emdvelox.py
+++ crucible/parsers/emdvelox.py
@@ -1,9 +1,11 @@
 from .base import BaseParser
 import os
-from ncempy.io import emdVelox # ncempy module
+from ncempy.io import emdVelox
 from crucible import BaseDataset
 
 class EMDVeloxParser(BaseParser):
+    _data_format = ".emd" # sets default self.data_format
+
     def parse(self):
         # Validate input
         if not self.files_to_upload:
@@ -20,34 +22,39 @@ class EMDVeloxParser(BaseParser):
 
     def upload_dataset(self, ingestor='ApiUploadIngestor', verbose=False, wait_for_ingestion_response=True):
         # upload the dataset for the entire file 
-        results = [super().upload_dataset(self, ingestor, verbose, wait_for_ingestion_response)]
+        file_upload_result = super().upload_dataset(self, ingestor, verbose, wait_for_ingestion_response)
+        file_dsid = file_upload_result['created_record']['unique_id'] # if using create_new_dataset instead of create_new_dataset_from_files, can directly use 
 
-        # create & upload the dataset for each measurement
-        detectors = [d['Detector'] for d in self.measurement_scientific_metadata] # get list of detectors
-        measurement_datasets = [BaseDataset(
-            unique_id      = self.mfid,
-            measurement    = self.measurement,
-            project_id     = self.project_id,
-            owner_orcid    = None,  # API key handles user authentication
-            dataset_name   = self.dataset_name,
-            session_name   = self.session_name,
-            public         = self.public,
-            instrument_name = detector,
-            data_format    = self.data_format,
-            source_folder  = self.source_folder,
-            file_to_upload = self.files_to_upload[0]
-        ) for detector in detectors]
+        # create & upload a dataset for each measurement
+        for md in self.measurement_scientific_metadata: 
+            detector = md['Detector'] # only works if BinaryResult is unpacked.
+            # create dataset
+            measurement_ds = BaseDataset(
+                # unique_id      = self.mfid, # need a new id for each measurement ds?
+                measurement    = detector,
+                project_id     = self.project_id,
+                owner_orcid    = None,  # API key handles user authentication
+                dataset_name   = self.dataset_name + f"({detector})",
+                session_name   = self.session_name,
+                public         = self.public,
+                instrument_name = self.instrument_name + f"({detector})",
+                data_format    = self.data_format,
+                source_folder  = self.source_folder,
+                file_to_upload = self.files_to_upload[0])
+        
+            # upload dataset to crucible: use create_new_dataset instead, if no file needed
+            measurement_upload_result = self.client.create_new_dataset_from_files(
+                measurement_ds,
+                files_to_upload=self.files_to_upload,
+                scientific_metadata=md,
+                keywords=self.keywords,
+                # get_user_info_function=self.client.get_user,
+                ingestor=ingestor,
+                verbose=verbose,
+                wait_for_ingestion_response=wait_for_ingestion_response)
+            measurement_dsid = measurement_upload_result['created_record']['unique_id']
 
-        # upload measurements to crucible
-        results.append([self.client.create_new_dataset_from_files(
-            measurement_datasets[i],
-            files_to_upload=self.files_to_upload,
-            scientific_metadata=self.measurement_scientific_metadata[i],
-            keywords=self.keywords,
-            # get_user_info_function=self.client.get_user,
-            ingestor=ingestor,
-            verbose=verbose,
-            wait_for_ingestion_response=wait_for_ingestion_response
-        ) for i in range(len(measurement_datasets))])
+            # link measurement dataset (child) with file dataset (parent)
+            self.client.link_datasets(file_dsid, measurement_dsid)
 
-        return results
+        return file_upload_result
