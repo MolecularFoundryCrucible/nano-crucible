ee8a58d8558413b6139d919164c2b9d38012559f
Catherine Tan <catherine95118@gmail.com>
2026-02-25 22:35:40 -0800

    temp: test dataset upload without file_upload

--- crucible/parsers/emdvelox.py
+++ crucible/parsers/emdvelox.py
@@ -15,9 +15,8 @@ from crucible import BaseDataset
 logger = logging.getLogger(__name__)
 
 class EMDVeloxParser(BaseParser):
-    _measurement = "EMD"
     _data_format = "EMD" 
-    _instrument_name = None # change?
+    _instrument_name = None # Spectre?
 
     def parse(self):
         # Validate input
@@ -33,41 +32,67 @@ class EMDVeloxParser(BaseParser):
         ncempy_datafile = emdVelox.fileEMDVelox(input_file)
 
         # custom attribute: measurement_scientific_metadata
-        # self.measurement_scientific_metadata = [ncempy_datafile.getMetadata(d) for d in ncempy_datafile.list_data]
+        # self.measurement_scientific_metadata = [ncempy_datafile.getMetadata(d) for d in ncempy_datafile.list_data] <- revert when ncempy version is updated
         self.measurement_scientific_metadata = [ncempy_datafile.parseMetaData(d) for d in ncempy_datafile.list_data]
 
     def upload_dataset(self, ingestor='ApiUploadIngestor', verbose=False, wait_for_ingestion_response=True):
-        # upload the dataset for the entire file 
-        file_upload_result = super().upload_dataset(ingestor, verbose, wait_for_ingestion_response)
+        upload_file = False # for testing purposes 
+
+        # upload the dataset for the entire file
+        if upload_file: 
+            file_upload_result = super().upload_dataset(ingestor, verbose, wait_for_ingestion_response)
+        else: 
+            file_upload_result = self.client.create_new_dataset(
+                self.to_dataset(),
+                scientific_metadata=self.scientific_metadata,
+                keywords=self.keywords,
+                verbose=verbose,
+            )
         file_dsid = file_upload_result['created_record']['unique_id'] # if using create_new_dataset instead of create_new_dataset_from_files, can directly use 
 
         # create & upload a dataset for each measurement
         for md in self.measurement_scientific_metadata: 
-            detector = md['Detector'] # only works if BinaryResult is unpacked.
+            detector = md['detectorName'] or "" # testing: using parseMetadata instead of getMetadata
+            # detector = md['Detector'] # only works if BinaryResult is unpacked. <- revert when ncempy version is updated
+
             # create dataset
             measurement_ds = BaseDataset(
                 # unique_id      = self.mfid, # need a new id for each measurement ds?
-                measurement    = detector,
+                measurement    = self.measurement, # how to determine this?
                 project_id     = self.project_id,
                 owner_orcid    = None,  # API key handles user authentication
-                dataset_name   = self.dataset_name + f"({detector})",
+                dataset_name   = (self.dataset_name or "") + f"({detector})",
                 session_name   = self.session_name,
                 public         = self.public,
-                instrument_name = self.instrument_name + f"({detector})",
+                instrument_name = self.instrument_name,
                 data_format    = self.data_format,
                 source_folder  = self.source_folder,
-                file_to_upload = self.files_to_upload[0])
+                # file_to_upload = self.files_to_upload[0] <- INCLUDE if upload_file
+            )
         
             # upload dataset to crucible: use create_new_dataset instead, if no file needed
-            measurement_upload_result = self.client.create_new_dataset_from_files(
-                measurement_ds,
-                files_to_upload=self.files_to_upload,
-                scientific_metadata=md,
-                keywords=self.keywords,
-                # get_user_info_function=self.client.get_user,
-                ingestor=ingestor,
-                verbose=verbose,
-                wait_for_ingestion_response=wait_for_ingestion_response)
+            
+            # pre-processing: remove AcquisitionTime from metadata <- remove when ncempy version is updated
+            md_to_upload = md
+            del md_to_upload["AcquisitionTime"] # DateTime is not JSON serializable
+            
+            if upload_file: 
+                measurement_upload_result = self.client.create_new_dataset_from_files(
+                    measurement_ds,
+                    files_to_upload=self.files_to_upload,
+                    scientific_metadata=md,
+                    keywords=self.keywords,
+                    ingestor=ingestor,
+                    verbose=verbose,
+                    wait_for_ingestion_response=wait_for_ingestion_response
+                )
+            else: 
+                measurement_upload_result = self.client.create_new_dataset(
+                    measurement_ds,
+                    scientific_metadata=md_to_upload,
+                    keywords=self.keywords,
+                    verbose=verbose
+                )
             measurement_dsid = measurement_upload_result['created_record']['unique_id']
 
             # link measurement dataset (child) with file dataset (parent)
