1c589b410bd5a8c507e0fc402bb2fc7bb53df140
Morgan Wall <mkywall3@gmail.com>
2026-03-06 14:04:28 -0800

    test fail

--- crucible/__init__.py
+++ crucible/__init__.py
@@ -4,7 +4,7 @@ nano-crucible: National Archive for NSRC Observations - Crucible
 Python client library for the Crucible API - the Molecular Foundry data lakehouse.
 """
 
-__version__ = "2.0.1"
+__version__ = "2.0.0"
 __author__ = "mkywall"
 
 import logging
