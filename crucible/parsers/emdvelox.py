d0fcb1dacd79ab0369d75fe6a78094c675f35a68
Catherine Tan <catherine95118@gmail.com>
2026-03-11 12:44:07 -0700

    implement nested upload for spectrum_image/processed_images

--- crucible/parsers/emdvelox.py
+++ crucible/parsers/emdvelox.py
@@ -27,43 +27,34 @@ class EMDVeloxParser(BaseParser):
         input_file = os.path.abspath(self.files_to_upload[0])
         if not os.path.exists(input_file):
             raise FileNotFoundError(f"File not found: {input_file}")
-
+        
         # Read input file using ncempy 
-        ncempy_datafile = emdVelox.fileEMDVeloxWithSpectra(input_file)
+        self._ncempy_datafile = emdVelox.fileEMDVeloxWithSpectra(input_file)
+        self._measurement_scientific_metadata = [self._ncempy_datafile.getMetadata(d) for d in self._ncempy_datafile.list_data]
 
-        # custom attribute: measurement_scientific_metadata
-        self.measurement_scientific_metadata = [ncempy_datafile.getMetadata(d) for d in ncempy_datafile.list_data] #<- revert when ncempy version is updated
-        # self.measurement_scientific_metadata = [ncempy_datafile.parseMetaData(d) for d in ncempy_datafile.list_data]
+        # Parse dataset_name and measurement for the file 
+        self.dataset_name = (self.dataset_name + " " if self.dataset_name else "") + os.path.basename(self.files_to_upload[0]) # append the filename to user-provided dataset_name
+        self.measurement = self._ncempy_datafile.getMeasurementType()
+        # prev: self.measurement = self._measurement_scientific_metadata[-1]['General']['groupType'] # last measurement's groupType options (in order): EDS_SI, Image (STEM or TEM), EDS_Spectrum
 
     def upload_dataset(self, ingestor='ApiUploadIngestor', verbose=False, wait_for_ingestion_response=True):
         upload_file = False # for testing purposes 
+         
+        get_groupType_from_md = lambda md: md['General']['groupType'] if 'groupType' in md['General'] else "" # options: STEM, TEM, EDS_Processed, EDS
+        get_title_from_md = lambda md: md['General']['title'] if 'title' in  md['General'] else "" # options: EDS_SI, EDS_Spectrum, HAADF, element name
 
-        # upload the dataset for the entire file
-        if upload_file: 
-            file_upload_result = super().upload_dataset(ingestor, verbose, wait_for_ingestion_response)
-        else: 
-            file_upload_result = self.client.create_new_dataset(
-                self.to_dataset(),
-                scientific_metadata=self.scientific_metadata,
-                keywords=self.keywords,
-                verbose=verbose,
-            )
-        file_dsid = file_upload_result['created_record']['unique_id'] # if using create_new_dataset instead of create_new_dataset_from_files, can directly use 
-
-        # create & upload a dataset for each measurement
-        for md in self.measurement_scientific_metadata: 
-            # detector = md['detectorName'] or "" # testing: using parseMetadata instead of getMetadata
-            detector = md['Detector'] # only works if BinaryResult is unpacked. <- revert when ncempy version is updated
-            measurement_field = md['General']['groupType'] if 'groupType' in  md['General'] else "" # TODO: VERIFY (STEM, TEM, EDS)
-            dataset_name = md['General']['title'] if 'title' in  md['General'] else "" # TODO: VERIFY, prev (detector)
-
-            # create dataset
+        def upload_measurement(md, parent_dsid): 
+            """
+            Uploads measurement dataset using measurment metadata MD. Links the created dataset record to PARENT_DSID.
+            Returns measurement_dsid. 
+            """
+            # Create dataset
             measurement_ds = BaseDataset(
                 # unique_id      = self.mfid, # need a new id for each measurement ds?
-                measurement    = measurement_field, 
+                measurement    = get_groupType_from_md(md), 
                 project_id     = self.project_id,
                 owner_orcid    = None,  # API key handles user authentication
-                dataset_name   = (self.dataset_name or "") + dataset_name + f"({detector})", # TODO: use metadata.title
+                dataset_name   = self.dataset_name + f" ({get_title_from_md(md)})", # TODO: use metadata.title
                 session_name   = self.session_name,
                 public         = self.public,
                 instrument_name = self.instrument_name, # TODO: include detector here? 
@@ -72,12 +63,7 @@ class EMDVeloxParser(BaseParser):
                 # file_to_upload = self.files_to_upload[0] <- INCLUDE if upload_file
             )
         
-            # upload dataset to crucible: use create_new_dataset instead, if no file needed
-            
-            # pre-processing: remove AcquisitionTime from metadata <- remove when ncempy version is updated
-            md_to_upload = md
-            # del md_to_upload["AcquisitionTime"] # DateTime is not JSON serializable
-            
+            # Upload dataset to crucible: use create_new_dataset instead, if no file needed            
             if upload_file: 
                 measurement_upload_result = self.client.create_new_dataset_from_files(
                     measurement_ds,
@@ -91,13 +77,40 @@ class EMDVeloxParser(BaseParser):
             else: 
                 measurement_upload_result = self.client.create_new_dataset(
                     measurement_ds,
-                    scientific_metadata=md_to_upload,
+                    scientific_metadata=md,
                     keywords=self.keywords,
                     verbose=verbose
                 )
             measurement_dsid = measurement_upload_result['created_record']['unique_id']
 
-            # link measurement dataset (child) with file dataset (parent)
-            self.client.link_datasets(file_dsid, measurement_dsid)
+            # Link measurement dataset (child) with file dataset (parent)
+            self.client.link_datasets(parent_dsid, measurement_dsid)
+            return measurement_dsid
 
+        # 1. upload the dataset for the entire file
+        if upload_file: 
+            file_upload_result = super().upload_dataset(ingestor, verbose, wait_for_ingestion_response)
+        else: 
+            file_upload_result = self.client.create_new_dataset(
+                self.to_dataset(),
+                scientific_metadata=self.scientific_metadata,
+                keywords=self.keywords,
+                verbose=verbose,
+            )
+        file_dsid = file_upload_result['created_record']['unique_id'] # if using create_new_dataset instead of create_new_dataset_from_files, can directly use 
+
+        # 2. upload measurement datasets 
+        spectrum_image_dsid = None
+
+        # iterate backwards through groups to catch if spectrum image exists (then handle nested uploads)
+        for i, md in enumerate(self._measurement_scientific_metadata[::-1]):
+            # expect that spectrum image will be at the end of measurement_scientific_metadata if it exists  
+            if i == 0 and get_groupType_from_md(md) == self._ncempy_datafile.SPECTRUM_IMAGE_GROUP_NAME:
+                spectrum_image_dsid = upload_measurement(md, file_dsid) # spectrum image dsid
+            else: 
+                # ensure that processed images are nested properly (assume: processed image exists => spectrum_image exists)
+                parent_dsid = spectrum_image_dsid if get_groupType_from_md(md) == self._ncempy_datafile.PROCESSED_IMAGE_GROUP_NAME else file_dsid
+                upload_measurement(md, parent_dsid)
+                
         return file_upload_result
+    
