913d0a9563b96ec9b77f93407076b2141153d2f4
roncofaber <fabrice.roncoroni@gmail.com>
2026-03-27 16:57:48 -0700

    Bump version to 2.1.0

--- crucible/__init__.py
+++ crucible/__init__.py
@@ -6,7 +6,7 @@ nano-crucible: National Archive for NSRC Observations - Crucible
 Python client library for the Crucible API - the Molecular Foundry data lakehouse.
 """
 
-__version__ = "2.0.2"
+__version__ = "2.1.0"
 __author__ = "mkywall"
 
 import logging
