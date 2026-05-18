a87f68c98f2f51e47674e8eac48562b78f893870
Morgan Wall <mkywall3@gmail.com>
2026-05-18 15:14:45 -0700

    bump the v

--- crucible/__init__.py
+++ crucible/__init__.py
@@ -6,7 +6,7 @@ nano-crucible: National Archive for NSRC Observations - Crucible
 Python client library for the Crucible API - the Molecular Foundry data lakehouse.
 """
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __author__ = "mkywall"
 
 import logging
